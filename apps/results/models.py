6e41ade087e315f26abfe5f9bf27bf0564040ebc
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-05 18:47:57 +0500

    results module

--- apps/results/models.py
+++ apps/results/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from apps.courses.models import Assignment, Task
+from apps.accounts.models import Student
+    
 
-# Create your models here.
+class Result(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='results')
+    assignment = models.ForeignKey(Assignment, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"{self.student.full_name} - {self.assignment.name}"
+    
+
+class Submission(models.Model):
+    attempts   = models.IntegerField(validators=[MinValueValidator(limit_value=0)])
+    is_correct = models.BooleanField()
+    task       = models.ForeignKey(Task, on_delete=models.CASCADE)
+    result     = models.ForeignKey(Result, on_delete=models.CASCADE, related_name='submissions')
+
+    def __str__(self):
+        return self.task.name
